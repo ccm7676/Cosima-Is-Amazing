6bb4d313fbde7080dc44ea26957effebe187b6b5
ccm7676 <50554024+ccm7676@users.noreply.github.com>
2020-08-30 21:50:25 +0200

    a

---
+++ script.js
@@ -0,0 +1,19 @@
+const h1 = document.querySelector("h1");
+const title = document.querySelector("title");
+const words = ["Amazing", "Epic", "Fantastic", "Incredibal", "Unbelivable", "Marvelous", "Phenomenal", "Legendary", "Sensational", "Miraculous", "Tremendous", "Supercalifragilisticexpialidocious"]
+var repititions = 30;
+var curr = 0;
+
+setTimeout(SetWord, 100)
+
+
+function SetWord(){
+    var randomNumber = Math.ceil(Math.random() * words.length); 
+    h1.innerHTML = words[randomNumber-1];
+    title.innerHTML = "You are " + words[randomNumber-1];
+    if(curr < repititions){
+        curr+=1;
+        setTimeout(SetWord,100);
+    }
+
+}
